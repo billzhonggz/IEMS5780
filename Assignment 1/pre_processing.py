8dd90d5a9d67bd9e7d378c211c14b8c084f32e03
Bill ZHONG <billzhonggz@outlook.com>
2019-10-03 13:15:26 +0800

    Defined combine function.

---
+++ Assignment 1/pre_processing.py
@@ -0,0 +1,12 @@
+"""pre_processing.py
+This is a part of the Assignment 1, IEMS5780, S1 2019-2020, CUHK
+by Junru Zhong
+Last modified Oct 3, 2019
+"""
+
+def combine(dataset_path):
+    """Combine the train and test dataset.
+    :param: path to the dataset: str
+    :return: combined dataset: pandas.Dataframe
+    """
+    pass
