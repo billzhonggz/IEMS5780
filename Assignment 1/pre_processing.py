2149e68d2c98a79f5bb4854454f633fa60bef084
Bill ZHONG <billzhonggz@outlook.com>
2019-10-06 23:23:11 +0800

    Add train-test-split.

--- Assignment 1/pre_processing.py
+++ Assignment 1/pre_processing.py
@@ -7,6 +7,9 @@ Last modified Oct 6, 2019
 import glob
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
+
 def combine(dataset_path, is_shuffle=False, save_path=None):
     """Combine the train and test dataset.
     :param: dataset_path: str
@@ -35,13 +38,17 @@ def combine(dataset_path, is_shuffle=False, save_path=None):
     # Shuffle
     if is_shuffle:
         df = df.sample(frac=1)
+    # Split the dataset
+    df_train, df_test = train_test_split(df, test_size=0.3)
     # Save DataFrame to csv file.
     if save_path is not None:
-        with open(save_path, 'w', encoding='utf8') as f:
-            df.to_csv(f)
+        with open(save_path + 'train.csv', 'w', encoding='utf8') as f:
+            df_train.to_csv(f)
+        with open(save_path + 'test.csv', 'w', encoding='utf8') as f:
+            df_test.to_csv(f)
     # Return the dataframe.
-    return df
+    return df_train, df_test
 
 
 if __name__ == '__main__':
-    combine('E:\\Datasets\\aclImdb\\', True, 'E:\\Datasets\\aclImdb\\combine.csv')
+    combine('E:\\Datasets\\aclImdb\\', True, 'E:\\Datasets\\aclImdb\\')
