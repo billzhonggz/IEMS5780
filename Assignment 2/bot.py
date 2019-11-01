1350a32f068560e7298c3a3624ef07bf34086b6d
Junru Zhong <billzhonggz@outlook.com>
2019-11-02 00:34:34 +0800

    Add multi-threading.

--- Assignment 2/bot.py
+++ Assignment 2/bot.py
@@ -6,25 +6,29 @@ import time
 import telepot
 import requests
 from telepot.loop import MessageLoop
+from threading import Thread
+from queue import Queue
 from io import BytesIO
 from PIL import Image
 
 
-def send_to_predict(image, chat_id):
+def send_to_predict(image, chat_id, output_queue):
     """Send images to the server.
-    :param image: PIL.Image object. Incoming image.
+    :param image: PIL image. The incoming images.
     :param chat_id: Telegram chat ID.
+    :param output_queue: Output prediction through a queue.
     :return str. List of predictions with probabilities. Sorted in descending order.
     """
     # Encode the image in base64.
     buffered = BytesIO()
     image.save(buffered, format='PNG')
     encoded_image = base64.b64encode(buffered.getvalue())
-    data = {'image': encoded_image, 'chat_id': chat_id}
-    data_send = json.dumps(data)
+    data_send = json.dumps(dict({'image': encoded_image.decode('ascii'), 'chat_id': chat_id}))
+    output_queue.put(data_send)
     # TODO: TCP client: encoded image send to the server. Waiting for receiving predictions.
-    predictions = ''
-    return predictions
+    # predictions = ''
+    # return predictions
+    return data_send
 
 
 def handle(msg):
@@ -33,31 +37,28 @@ def handle(msg):
     recevied by the bot
     """
     content_type, chat_type, chat_id = telepot.glance(msg)
+    logging.info('Handling incoming message {}.'.format(chat_id))
+    # Initialize the queue.
+    output_queue = Queue()
 
     if content_type == "text":
         content = msg["text"]
-        # Try to access the URL.
-        try:
-            # Find URL as a substring.
-            url = re.search('[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)',
-                            content)
-        except AttributeError:
-            # If the incoming message does not contain a URL.
-            help_info = 'To try out the image classification, please send an image or a image URL instead.'
-            reply = "You said: {}\n{}".format(content, help_info)
-            bot.sendMessage(chat_id, reply)
-            return
         # Try to download URL.
         try:
-            image_response = requests.get(url)
+            image_response = requests.get(content)
             if image_response.status_code != 200:
                 raise Exception('Response code is not 200 OK.')
             if 'image' not in image_response.headers['content-type']:
                 raise Exception('The URL does not contains an image.')
             i = Image.open(BytesIO(image_response.content))
             # Pass to predicting server.
-            # TODO: change to a queue.
-            predictions = send_to_predict(i, chat_id)
+            # Feedback to user.
+            bot.sendMessage(chat_id, 'Predicting...')
+            Thread(target=send_to_predict, args=(i, chat_id, output_queue)).start()
+            # TODO: add a blocking.
+            # Get the result.
+            with not output_queue.empty():
+                predictions = output_queue.get()
             # Return predictions to the client.
             bot.sendMessage(chat_id, predictions)
         except Exception as e:
@@ -76,8 +77,11 @@ def handle(msg):
             # Feedback to user.
             bot.sendMessage(chat_id, 'Predicting...')
             # Pass to predicting server.
-            # TODO: change to a queue.
-            predictions = send_to_predict(i, chat_id)
+            Thread(target=send_to_predict, args=(i, chat_id, output_queue)).start()
+            # TODO: add a blocking.
+            # Get the result.
+            if not output_queue.empty():
+                predictions = output_queue.get()
             # Return predictions to the client.
             bot.sendMessage(chat_id, predictions)
         except Exception as e:
@@ -87,9 +91,10 @@ def handle(msg):
 
 
 if __name__ == "__main__":
-
+    logging.basicConfig(level=logging.DEBUG)
     # Provide your bot's token
     bot = telepot.Bot("829334217:AAHdT50M-1SejyMNa8Wug2KlDJThvp5Fxwc")
+    logging.info('Bot script starting...')
     MessageLoop(bot, handle).run_as_thread()
 
     while True:
