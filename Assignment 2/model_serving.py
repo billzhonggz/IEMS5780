2c2a11ffea347b010306b87b4adf9d92d8fcd17b
Junru Zhong <billzhonggz@outlook.com>
2019-11-07 12:52:48 +0800

    Adapting example code to model serving.

--- Assignment 2/model_serving.py
+++ Assignment 2/model_serving.py
@@ -11,11 +11,58 @@ import torch
 import json
 import requests
 import logging
+import socket
 import torchvision.models as models
 import torchvision.transforms as transforms
 
 from torch.autograd import Variable
+from threading import Thread
 from PIL import Image
+
+
+def get_logger():
+    """Initialize logger. Copy from sample code on course website.
+    :return logging.Logger.
+    """
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s, %(threadName)s, [%(levelname)s] : %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+def serve_client(client_socket, address, model, labels):
+    """Decodes the message from client, call prediction function. Send back prediction result.
+    Adapted from the sample code on course website.
+    :param client_socket: socket. Socket object contains client request.
+    :param address: str. Client address.
+    :param model: PyTorch model object.
+    :param labels: str in JSON format. Prediction labels.
+    """
+    logger.info("Serving client from {}".format(address))
+    # TODO: Modify to matching terminating pattern rather than fix length.
+    data = client_socket.recv(1024)
+    # TODO: Decode image, send to predict, then get the result.
+    client_socket.sendall(data)
+    client_socket.close()
+    logger.info("Finished")
+
+
+def init_model():
+    """Download PyTorch model, load to memory.
+    :return: PyTorch model object.
+    :return: Labels in JSON format.
+    """
+    # Load model
+    model = models.inception_v3(pretrained=True)
+    # Download labels.
+    labels_content = requests.get(
+        'https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json').text
+    labels = json.loads(labels_content)
+    return model, labels
 
 
 def do_predict(model, labels, image):
@@ -52,21 +99,28 @@ def do_predict(model, labels, image):
     out = []
     # Top-5 predictions.
     for score, label in predictions[:5]:
-        # DEBUG: print the prediction scores.
-        print('{:16s}: {:.4f}'.format(label, score))
         out += [{'label': label, 'proba': score}]
     return out
 
 
 if __name__ == '__main__':
+    # Set port number.
+    PORT_NUMBER = 8888
     # Enable logging.
-    logging.basicConfig(level=logging.DEBUG)
-    # Load model
-    model = models.inception_v3(pretrained=True)
-    # Download labels.
-    labels_content = requests.get('https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json').text
-    labels = json.loads(labels_content)
-    # DEBUG: sample image.
-    image = Image.open('image.jpg')
-    # Call function
-    do_predict(model, labels, image)
+    logger = get_logger()
+    # Init model.
+    model, labels = init_model()
+    logger.info('Model initialized.')
+    # Start TCP server.
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("localhost", PORT_NUMBER))
+    server_socket.listen(10)
+    logger.info("Server starts listening for connection...")
+    # Accepting client connections.
+    while True:
+        (client_socket, address) = server_socket.accept()
+        logger.info("Accepted connection from {}".format(address))
+        client_thread = Thread(target=serve_client,
+                               args=(client_socket, address, model, labels),
+                               daemon=True)
+        client_thread.start()
